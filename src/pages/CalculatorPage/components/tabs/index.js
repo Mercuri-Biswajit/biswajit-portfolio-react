99e31295e75e1ba52e72c0539bb7d90b8a359758
mercuri-biswajit <bisu.rnj@gmail.com>
2026-02-22 10:02:58 +0530

    remove calculatorpage

--- src/pages/CalculatorPage/components/tabs/index.js
+++
@@ -1,4 +0,0 @@
-// Re-export all costing result sub-tab components
-export { CostBreakdown } from "./CostBreakdown";
-export { StructureDesign } from "./StructureDesign";
-export { StaircaseDesign, FoundationDesign, CompleteBBS } from "./OtherTabs";
